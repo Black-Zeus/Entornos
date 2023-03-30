f6c97c8b1d0de25c7550c274c6bc287052af8600
Black-Zeus <43542648+Black-Zeus@users.noreply.github.com>
2023-03-30 00:47:27 -0300

    optimizacion

--- archLinux.sh
+++ archLinux.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
+# Verificar si el script se está ejecutando como root
 if [ $(id -u) = 0 ]; then
   echo "Este script no debe ser ejecutado como root"
   exit 1
 fi
 
-echo "Instalando paquetes del sistema..."
+# Actualizar e instalar paquetes del sistema
 sudo pacman -Syu
-sudo pacman -S --noconfirm base base-devel net-tools open-vm-tools 
-sudo pacman -S --noconfirm bspwm sxhkd polybar rofi picom kitty feh 
+sudo pacman -S --noconfirm base base-devel net-tools open-vm-tools bspwm sxhkd polybar rofi picom kitty feh zsh git zip unrar p7zip unzip bat lsd wget curl firefox caja vi vim neovim htop btop mlocate
 
-sudo pacman -S --noconfirm zsh git zip unrar p7zip unzip bat lsd wget curl
-sudo pacman -S --noconfirm firefox caja vi vim neovim htop btop
+# Actualizar base de datos de archivos
+sudo updatedb
 
+# Crear directorios necesarios
+mkdir -p ~/.config/{bspwm,sxhkd,polybar} ~/{WallPapers,ConfigFiles}
 
-mkdir -p ~/.config/bspwm
-mkdir -p ~/.config/sxhkd
-mkdir -p ~/.config/polybar
+# Copiar archivos de configuración bspwm y sxhkd
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
-cp /usr/share/doc/bspwm/example/bspwmrc ~/.config/bspwm/bspwmrc
-cp /usr/share/doc/bspwm/example/sxhkdrc ~/.config/sxhkd/sxhkdrc
+# Agregar personalizaciones al archivo de configuración bspwmrc
+cat <<EOF >> ~/.config/bspwm/bspwmrc
+bspc config border_width 1
+/home/zeus/.config/polybar/./launch.sh
+picom &
+feh --bg-fill ~/WallPapers/Wall_OnePiece.png &
+EOF
 
+# Descargar imagen de fondo de pantalla y archivo de configuración
+curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png -o ~/WallPapers/Wall_OnePiece.png
+curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip | tar -xz -C ~/ConfigFiles/ && rm ~/ConfigFiles/config.zip
 
-echo "###\n\
-# Scrip para lanzar la Polybar al iniciar sesion\n\
-/home/zeus/.config/polybar/./launch.sh\n\
-picom &\n\
-bspc config border_width 1\n\n\
-#Parametros personalizados\n\
-#Adicion Fondo de Pantalla\n\
-feh --bg-fill /usr/share/custonTheme/hell_wallpaper.jpg &" >> ~/.config/bspwm/bspwmrc
-
-
-sudo mkdir -p /usr/share/custonTheme/ && mkdir -p ~/.config/ && mkdir -p ~/ConfigFiles/
-
-sudo curl https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png > /usr/share/custonTheme/Wall_OnePiece.png
-wget https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip -o ~/ConfigFiles/
-
+# Descomprimir archivo de configuración y eliminar archivo zip
 unzip ~/ConfigFiles/config.zip
 rm -Rf ~/ConfigFiles/config.zip
