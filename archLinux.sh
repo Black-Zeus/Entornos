98eabad05e65c873ae3cde3c149ea72de0649dc6
Black-Zeus <43542648+Black-Zeus@users.noreply.github.com>
2023-03-29 23:28:21 -0300

    Update archLinux.sh

--- archLinux.sh
+++ archLinux.sh
@@ -5,69 +5,37 @@ if [ $(id -u) = 0 ]; then
   exit 1
 fi
 
-
 echo "Instalando paquetes del sistema..."
 sudo pacman -Syu
-sudo pacman -S --noconfirm base base-devel net-tools open-vm-tools htop btop neofetch openssh
+sudo pacman -S --noconfirm base base-devel net-tools open-vm-tools 
+sudo pacman -S --noconfirm bspwm sxhkd polybar rofi picom kitty feh 
 
-echo "Instalando paquetes de ventanas..."
-sudo pacman -S --noconfirmbspwm sxhkd polybar rofi picom kitty feh wget curl
+sudo pacman -S --noconfirm zsh git zip unrar p7zip unzip bat lsd wget curl
+sudo pacman -S --noconfirm firefox caja vi vim neovim htop btop
 
-echo "Instalando paquetes de aplicaciones..."
-sudo pacman -S --noconfirmzsh caja firefox google-chrome remmina remmina-plugin git
 
-echo "Instalando paquetes de compresión..."
-sudo pacman -S --noconfirmzip unrar p7zip
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
+mkdir -p ~/.config/polybar
 
-echo "Instalando NeoVim y Powerlevel10k..."
+cp /usr/share/doc/bspwm/example/bspwmrc ~/.config/bspwm/bspwmrc
+cp /usr/share/doc/bspwm/example/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
-# Instalar NeoVim y el plugin Powerlevel10k
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-echo "Instalando fuentes TrueType de Windows..."
+echo "###\n\
+# Scrip para lanzar la Polybar al iniciar sesion\n\
+/home/zeus/.config/polybar/./launch.sh\n\
+picom &\n\
+bspc config border_width 1\n\n\
+#Parametros personalizados\n\
+#Adicion Fondo de Pantalla\n\
+feh --bg-fill /usr/share/custonTheme/hell_wallpaper.jpg &" >> ~/.config/bspwm/bspwmrc
 
-# Instalar fuentes TrueType de Windows
-mkdir -p ~/.local/share/fonts/windows
-cd ~/.local/share/fonts/windows
-curl -LO https://github.com/microsoft/cascadia-code/releases/download/v2103.17/CascadiaCode-2103.17.zip
-unzip CascadiaCode-2103.17.zip
-fc-cache -f -v
 
-echo "Instalando fuentes Nerd Fonts Hack..."
+sudo mkdir -p /usr/share/custonTheme/ && mkdir -p ~/.config/ && mkdir -p ~/ConfigFiles/
 
-# Instalar fuentes Nerd Fonts Hack
-mkdir -p ~/.local/share/fonts/nerd-fonts
-cd ~/.local/share/fonts/nerd-fonts
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
-unzip Hack.zip
-fc-cache -f -v
+sudo curl https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png > /usr/share/custonTheme/Wall_OnePiece.png
+wget https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip -o ~/ConfigFiles/
 
-echo "Descargando y descomprimiendo el archivo ConfigFile.zip..."
-
-# Descargar y descomprimir ConfigFile.zip
-# Verificar si el directorio ~/.config/ existe, y si no, crearlo
-if [ ! -d "$HOME/.config/" ]; then
-  mkdir -p "$HOME/.config/"
-fi
-
-cd ~/.config
-curl -LO https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip
-unzip ConfigFile.zip
-
-# Aplicar permisos de ejecución a todos los archivos .sh dentro de todos los subdirectorios de ~/.config/
-echo "Permisos de ejecución aplicados a todos los archivos .sh dentro de los subdirectorios de ~/.config/"
-find "$HOME/.config/" -type f -name "*.sh" -exec chmod +x {} \;
-
-# Descargo Fondo de Pantalla
-echo "Descargamos fondo de pantalla"
-cd ~/.config
-wget https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png
-
-# Creamos directorio almacen para Fondo de Pantalla
-mkdir -p /usr/share/custonTheme/
-sudo mv ~/.config/Wall_OnePiece.png /usr/share/custonTheme/Wall_OnePiece.png
-
-echo "¡Listo! La instalación ha finalizado."
+unzip ~/ConfigFiles/config.zip
+rm -Rf ~/ConfigFiles/config.zip
