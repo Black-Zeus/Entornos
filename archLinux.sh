10bb374e6145145d39e4702199b26f0c4c936c29
Black-Zeus <43542648+Black-Zeus@users.noreply.github.com>
2023-03-30 22:40:39 -0300

    update

--- archLinux.sh
+++ archLinux.sh
@@ -6,21 +6,21 @@ if [ $(id -u) = 0 ]; then
   exit 1
 fi
 
-# Actualizar e instalar paquetes del sistema
+echo "Actualizando e instalando paquetes del sistema..."
 sudo pacman -Syu
 sudo pacman -S --noconfirm base base-devel net-tools open-vm-tools bspwm sxhkd polybar rofi picom kitty feh zsh git zip unrar p7zip unzip bat lsd wget curl firefox caja vi vim neovim htop btop mlocate
 
-# Actualizar base de datos de archivos
+echo "Actualizando la base de datos de archivos..."
 sudo updatedb
 
-# Crear directorios necesarios
+echo "Creando directorios necesarios..."
 mkdir -p ~/.config/{bspwm,sxhkd,polybar} ~/{WallPapers,ConfigFiles}
 
-# Copiar archivos de configuración bspwm y sxhkd
+echo "Copiando archivos de configuración bspwm y sxhkd..."
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
-# Agregar personalizaciones al archivo de configuración bspwmrc
+echo "Agregando personalizaciones al archivo de configuración bspwmrc..."
 cat <<EOF >> ~/.config/bspwm/bspwmrc
 # Configuración de bspwm
 # Se establece el ancho del borde de las ventanas en 1 píxel
@@ -39,28 +39,41 @@ picom &
 feh --bg-fill ~/WallPapers/Wall_OnePiece.png &
 EOF
 
-# Este script cambia la terminal por defecto de urxvt a kitty en el archivo sxhkdrc de sxhkd.
-# También cambia la combinación de teclas super + @space a super + d y el programa asociado a rofi -show run.
-
-# Primero, reemplazamos 'urxvt' por 'kitty' en el archivo sxhkdrc.
+echo "Cambiando la terminal predeterminada de urxvt a kitty..."
 sed -i 's/urxvt/kitty/g' ~/.config/sxhkd/sxhkdrc
 
-# A continuación, reemplazamos 'super + @space' por 'super + d' y 'dmenu_run' por 'rofi -show run'.
+echo "Cambiando la combinación de teclas y el programa asociado en sxhkdrc..."
 sed -i 's/super + @space/super + d/g' ~/.config/sxhkd/sxhkdrc
 sed -i 's/dmenu_run/rofi -show run/g' ~/.config/sxhkd/sxhkdrc
 
+echo "Descargando imagen de fondo de pantalla y archivo de configuración..."
+curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entorn
 
-# Descargar imagen de fondo de pantalla y archivo de configuración
+echo "Descargar imagen de fondo de pantalla y archivo de configuración"
+echo "Se descarga una imagen de fondo de pantalla y un archivo de configuración desde GitHub"
+echo "usando el comando curl y se guardan en las carpetas correspondientes en el sistema de archivos"
 curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png -o ~/WallPapers/Wall_OnePiece.png
 curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip -o ~/ConfigFiles/config.zip
 
-# Descomprimir archivo de configuración y eliminar archivo zip
+echo "Descomprimir archivo de configuración y eliminar archivo zip"
+echo "Se descomprime el archivo de configuración descargado usando el comando unzip"
+echo "y se elimina el archivo zip para ahorrar espacio en el disco."
 cd ~/ConfigFiles/
 unzip config.zip
 rm -Rf config.zip
 
+echo "Copiar archivos de configuración de polybar y bin"
+echo "Se copian los archivos de configuración de polybar y bin del archivo descomprimido"
+echo "a sus respectivas carpetas en el sistema de archivos."
 cp -r ~/ConfigFiles/polybar/ ~/.config/polybar
 cp -r ~/ConfigFiles/bin ~/.config/bin
 
-find ~/.config -type f -name "*.sh" -exec chmod +x {} \;
+echo "Dar permisos de ejecución a los scripts"
+echo "Se otorgan permisos de ejecución a todos los archivos .sh que se encuentran en la carpeta ~/.config"
+echo "para que puedan ser ejecutados sin problemas."
+find ~/.config -type f -name "*.sh" -exec chmod +x {} ;
+
+echo "Cambiar shell predeterminada a zsh"
+echo "Se cambia la shell predeterminada del usuario a zsh usando el comando chsh"
+echo "para poder usar todas las características de la shell zsh."
 chsh -s /bin/zsh
