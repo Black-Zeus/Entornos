337e8d9b712f06bf51029b4dd8cd0a84ccd71e28
Black-Zeus <43542648+Black-Zeus@users.noreply.github.com>
2023-04-01 22:48:18 -0300

    update

--- archLinux.sh
+++ archLinux.sh
@@ -1,83 +1,40 @@
 #!/bin/bash
 
-# Verificar si el script se está ejecutando como root
 if [ $(id -u) = 0 ]; then
   echo "Este script no debe ser ejecutado como root"
   exit 1
 fi
 
 echo "Actualizando e instalando paquetes del sistema..."
-sudo pacman -Syu --noconfirm 
+sudo pacman -Syu --needed --noconfirm 
 
-# Instalamos la interfaz Gráfica
-sudo pacman -Sy --noconfirm xorg xorg-server gnome
+echo "Instalando la interfaz gráfica..."
+sudo pacman -Sy --needed --noconfirm xorg xorg-server gnome
 sudo systemctl enable gdm
 
-sudo pacman -Sy --noconfirm gtkmm open-vm-tools xf86-video-vmware xf86-input-vmmouse
+echo "Instalando paquetes adicionales..."
+sudo pacman -Sy --needed --noconfirm gtkmm open-vm-tools xf86-video-vmware xf86-input-vmmouse
 sudo systemctl enable vmtoolsd
-
-sudo pacman -Sy --noconfirm base base-devel net-tools bspwm sxhkd polybar rofi picom kitty feh zsh git zip unrar p7zip unzip bat lsd wget curl firefox caja vi vim neovim htop btop mlocate
+sudo pacman -Sy --needed --noconfirm base base-devel net-tools bspwm sxhkd polybar rofi picom kitty feh zsh git zip unrar p7zip unzip bat lsd wget curl firefox caja vi vim neovim htop btop mlocate
 
 echo "Actualizando la base de datos de archivos..."
 sudo updatedb
 
 echo "Creando directorios necesarios..."
 mkdir -p ~/.config/{bspwm,sxhkd,polybar} ~/{WallPapers,ConfigFiles,GitRepos,Desktop,powerlevel10k}
-sudo mkdir -p /usr/share/zsh-autosuggestions/ /usr/share/zsh-sudo/ /usr/share/zsh-syntax-highlighting/ 
-
-echo "Copiando archivos de configuración bspwm y sxhkd..."
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
-
-echo "Agregando personalizaciones al archivo de configuración bspwmrc..."
-cat <<EOF >> ~/.config/bspwm/bspwmrc
-# Configuración de bspwm
-# Se establece el ancho del borde de las ventanas en 1 píxel
-bspc config border_width 1
-
-# Ejecución de polybar
-# Se ejecuta el script de inicio de polybar, que se encuentra en la ruta especificada
-/home/zeus/.config/polybar/./launch.sh
-
-# Compositor de ventanas
-# Se ejecuta picom, que es un compositor de ventanas que proporciona transparencia
-picom &
-
-# Establecimiento del fondo de pantalla
-# Se establece el fondo de pantalla con una imagen específica, que se encuentra en la ruta especificada
-feh --bg-fill ~/WallPapers/Wall_OnePiece.png &
-EOF
-
-echo "Cambiando la terminal predeterminada de urxvt a kitty..."
-sed -i 's/urxvt/kitty/g' ~/.config/sxhkd/sxhkdrc
-
-echo "Cambiando la combinación de teclas y el programa asociado en sxhkdrc..."
-sed -i 's/super + @space/super + d/g' ~/.config/sxhkd/sxhkdrc
-sed -i 's/dmenu_run/rofi -show run/g' ~/.config/sxhkd/sxhkdrc
+sudo mkdir -p /usr/share/zsh-autosuggestions/ /usr/share/zsh-sudo/ /usr/share/zsh-syntax-highlighting/
 
 echo "Descargando imagen de fondo de pantalla y archivo de configuración..."
-curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entorn
-
-echo "Descargar imagen de fondo de pantalla y archivo de configuración"
-echo "Se descarga una imagen de fondo de pantalla y un archivo de configuración desde GitHub"
-echo "usando el comando curl y se guardan en las carpetas correspondientes en el sistema de archivos"
 curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/Wall_OnePiece.png -o ~/WallPapers/Wall_OnePiece.png
 curl -sfL https://raw.githubusercontent.com/Black-Zeus/Entornos/main/config.zip -o ~/ConfigFiles/config.zip
 
-echo "Descomprimir archivo de configuración y eliminar archivo zip"
-echo "Se descomprime el archivo de configuración descargado usando el comando unzip"
-echo "y se elimina el archivo zip para ahorrar espacio en el disco."
+echo "Descomprimiendo archivo de configuración y eliminando archivo zip..."
 cd ~/ConfigFiles/
 unzip config.zip
-rm -Rf config.zip
 
-echo "Copiar archivos de configuración de polybar y bin"
-echo "Se copian los archivos de configuración de polybar y bin del archivo descomprimido"
-echo "a sus respectivas carpetas en el sistema de archivos."
-
+echo "Copiando archivos de configuración de polybar y bin..."
 cp -r ~/ConfigFiles/bin ~/.config/
 cp -r ~/ConfigFiles/zshrc/zshrc ~/.zshrc
-
 cp -r ~/ConfigFiles/polybar ~/.config/
 cp -r ~/ConfigFiles/bspwm ~/.config/
 cp -r ~/ConfigFiles/kitty ~/.config/
@@ -86,18 +43,19 @@ cp -r ~/ConfigFiles/picom ~/.config/
 cp -r ~/ConfigFiles/rofi ~/.config/
 cp -r ~/ConfigFiles/sxhkd ~/.config/
 
-cp -r ~/ConfigFiles/powerlevel10k ~/powerlevel10k
+echo "Copiando archivos de powerlevel10k y zsh_modul..."
+cp -r ~/ConfigFiles/powerlevel10k ~/
 sudo cp -r ~/ConfigFiles/zsh_modul/zsh-* /usr/share/
 
+echo "Actualizando configuración..."
+sed -i "s/alias cat='batcat'/alias cat='bat'/" ~/.zshrc
+sed -i "s+/opt/kitty/bin/kitty+$(which kitty)+" ~/.config/sxhkd/sxhkd
+sed -i "s+//usr/share/custonTheme/hell_wallpaper.jpg+~/WallPapers/Wall_OnePiece.png+" ~/.config/bspwm/bspwmrc
 
-echo "Dar permisos de ejecución a los scripts"
-echo "Se otorgan permisos de ejecución a todos los archivos .sh que se encuentran en la carpeta ~/.config"
-echo "para que puedan ser ejecutados sin problemas."
+echo "Otorgando permisos de ejecución..."
 find ~/.config -type f -name "*.sh" -exec chmod +x {} \;
 
-echo "Cambiar shell predeterminada a zsh"
-echo "Se cambia la shell predeterminada del usuario a zsh usando el comando chsh"
-echo "para poder usar todas las características de la shell zsh."
+echo "Cambiando shell predeterminada..."
 chsh -s $(which zsh)
 
 echo "Instalando fuentes Nerd Fonts Hack..."
@@ -107,19 +65,17 @@ sudo mkdir -p /usr/share/fonts/nerd-fonts
 cd /usr/share/fonts/nerd-fonts
 sudo curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
 sudo unzip Hack.zip
-sudo rm -Rf *.zip *.md
 fc-cache -f -v
 
-
 # Instalando AUR
-
 cd ~/GitRepos
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
 
-
-## C
+# Eliminar archivos Innecesarios
+sudo rm -rf /usr/share/fonts/nerd-fonts/{*.zip,*.md}
+rm -rf ~/ConfigFiles/config.zip
 
 # Terminamos la instalacion
 echo "¡Listo! La instalación ha finalizado."
