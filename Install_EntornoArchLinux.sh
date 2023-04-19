b389b660ea8399f92d4b31502c92139d61493f16
Black-Zeus <43542648+Black-Zeus@users.noreply.github.com>
2023-04-19 17:51:43 -0400

    Update Install_EntornoArchLinux.sh

--- Install_EntornoArchLinux.sh
+++ Install_EntornoArchLinux.sh
@@ -8,23 +8,58 @@ fi
 echo "Actualizando e instalando paquetes del sistema..."
 sudo pacman -Syu --noconfirm 
 
-echo "Paquetes de gráficos y servidor X"
-sudo pacman -Sy --needed --noconfirm xorg xorg-server
 
-echo "Entorno de escritorio GNOME"
-sudo pacman -Sy --needed --noconfirm gnome
+# Preguntar al usuario qué entorno de escritorio desea instalar
+echo "¿Qué entorno de escritorio deseas instalar?"
+echo "1) Xfce"
+echo "2) GNOME"
+read -r choice
 
-echo "Habilitando servicio de GDM..."
-sudo systemctl enable gdm
+# Validar la opción ingresada
+while [[ "$choice" != "1" && "$choice" != "2" ]]; do
+  echo "Opción inválida. Por favor, ingresa 1 para instalar Xfce o 2 para instalar GNOME."
+  read -r choice
+done
 
-echo "Paquetes de interfaz gráfica"
-sudo pacman -Sy --needed --noconfirm gtkmm
+# Instalar el entorno de escritorio correspondiente según la opción del usuario
+sudo pacman -Sy --needed --noconfirm xorg xorg-server 
+if [ "$choice" = "1" ]; then
+  # Instalar Xfce y Thunar con algunos plugins útiles
+  sudo pacman -Sy --needed --noconfirm xfce4 thunar thunar-volman thunar-archive-plugin thunar-shares-plugin thunar-media-tags-plugin thunar-vcs-plugin gvfs
+  
+  # Habilitar el servicio de LightDM, que es el gestor de sesiones de Xfce
+  sudo systemctl enable lightdm.service
+elif [ "$choice" = "2" ]; then
+  # Instalar GNOME y GDM
+  sudo pacman -Sy --needed --noconfirm gnome gdm gtkmm
+  
+  # Habilitar el servicio de GDM, que es el gestor de sesiones de GNOME
+  sudo systemctl enable gdm
+fi
 
-echo "Paquetes para virtualización"
-sudo pacman -Sy --needed --noconfirm open-vm-tools xf86-video-vmware xf86-input-vmmouse
 
-echo "Habilitando servicio de vmtoolsd..."
-sudo systemctl enable vmtoolsd
+# Preguntar al usuario si desea instalar las herramientas de integración
+echo "¿Deseas instalar las herramientas de integración de VMware?"
+echo "S) Sí"
+echo "N) No"
+read -r choice
+
+# Validar la opción ingresada
+while [[ "$choice" != "S" && "$choice" != "N" ]]; do
+  echo "Opción inválida. Por favor, ingresa S para instalar las herramientas de integración o N para omitir esta opción."
+  read -r choice
+done
+
+# Instalar las herramientas de integración si el usuario lo desea
+if [ "$choice" = "S" ]; then
+  echo "Paquetes para virtualización"
+	sudo pacman -Sy --needed --noconfirm open-vm-tools xf86-video-vmware xf86-input-vmmouse
+
+	echo "Habilitando servicio de vmtoolsd..."
+	sudo systemctl enable vmtoolsd
+fi
+
+
 
 echo "Instalando paquetes base..."
 sudo pacman -Sy --needed --noconfirm base base-devel
@@ -33,7 +68,24 @@ echo "Instalando herramientas de red..."
 sudo pacman -Sy --needed --noconfirm net-tools
 
 echo "Instalando gestores de ventanas y escritorios..."
-sudo pacman -Sy --needed --noconfirm bspwm sxhkd polybar  #picom
+sudo pacman -Sy --needed --noconfirm bspwm sxhkd polybar 
+
+# Preguntar al usuario si desea instalar picom
+echo "¿Deseas instalar Picom?"
+echo "S) Sí"
+echo "N) No"
+read -r choice_picom
+
+# Validar la opción ingresada
+while [[ "$choice_picom" != "S" && "$choice_picom" != "N" ]]; do
+  echo "Opción inválida. Por favor, ingresa S para instalar Picom o N para omitir esta opción."
+  read -r choice_picom
+done
+
+# Instalar picom si el usuario lo desea
+if [ "$choice_picom" = "S" ]; then
+  sudo pacman -Sy --needed --noconfirm picom
+fi
 
 echo "Instalando terminales..."
 sudo pacman -Sy --needed --noconfirm kitty zsh
@@ -42,16 +94,13 @@ echo "Instalando herramientas de compresión y descompresión..."
 sudo pacman -Sy --needed --noconfirm zip unrar p7zip unzip
 
 echo "Instalando herramientas de visualización de archivos..."
-sudo pacman -Sy --needed --noconfirm bat lsd feh pluma
-
-echo "Instalando gestor de archivos..."
-sudo pacman -Sy --needed --noconfirm caja
+sudo pacman -Sy --needed --noconfirm bat lsd feh
 
 echo "Instalando editores de texto..."
 sudo pacman -Sy --needed --noconfirm vi vim neovim
 
 echo "Instalando herramientas de descarga..."
-sudo pacman -Sy --needed --noconfirm wget curl
+sudo pacman -Sy --needed --noconfirm wget curl git
 
 echo "Instalando navegador web..."
 sudo pacman -Sy --needed --noconfirm firefox
@@ -59,14 +108,43 @@ sudo pacman -Sy --needed --noconfirm firefox
 echo "Instalando herramientas de monitoreo del sistema..."
 sudo pacman -Sy --needed --noconfirm htop btop mlocate
 
+echo "Instalando herramientas de Audio"
+sudo pacman -S --needed --noconfirm pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils pamixer 
+
+echo "Instalando herramientas de Brillo"
+sudo pacman -S --needed --noconfirm brightnessctl 
+
+echo "Instalando herramientas de Notificaciones"
+sudo pacman -S --needed --noconfirm dunst dunstify 
+
+echo "Instalando herramientas de Como gestor de aerchivo, cambiar caja por thunair"
+sudo pacman -S --needed --noconfirm thunar thunar-archive-plugin  rofi
+
+#echo "Instalando herramientas de Bloqueo de pantalla"
+#sudo pacman -S --needed --noconfirm betterlockscreen xautolock
+
+echo "Instalando herramientas de Clipboard avanzado"
+sudo pacman -S --needed --noconfirm xclip
+
+echo "Instalando herramientas de Varias"
+sudo pacman -S --needed --noconfirm cmatrix pinta
+
+echo "Instalando herramientas Picom"
+sudo pacman -S --needed --noconfirm picom
+
+echo "Instalacion de paru Package AUR (Arch User Repository) "
+git clone https://aur.archlinux.org/paru.git -C /tmp
+cd /tmp/paru
+makepkg -si
+
 echo "Actualizando la base de datos de archivos..."
 sudo updatedb
 
 echo "Creando directorios necesarios..."
 
 # Directorios en el directorio personal
-config_dirs=(polybar rofi nvim sxhkd bspwm kitty)
-personal_dirs=(WallPapers ConfigFiles GitRepos powerlevel10k)
+config_dirs=(polybar rofi nvim sxhkd bspwm kitty dunst picom bin)
+personal_dirs=(WallPapers ConfigFiles powerlevel10k)
 system_dirs=(zsh-autosuggestions zsh-sudo zsh-syntax-highlighting fonts/nerd-fonts  fonts/polybar)
 
 echo "Creando Directorios de Configuracion"
@@ -112,27 +190,22 @@ cp -r ~/ConfigFiles/bin ~/.config/
 cp -r ~/ConfigFiles/zshrc/zshrc ~/.zshrc
 cp -r ~/ConfigFiles/p10k/p10k.zsh  ~/.p10k.zsh
 cp -r ~/ConfigFiles/polybar/* ~/.config/polybar
-cp -r ~/ConfigFiles/rofi/* ~/.config/rofi
 cp -r ~/ConfigFiles/nvim/* ~/.config/nvim
 cp -r ~/ConfigFiles/sxhkd/* ~/.config/sxhkd
 cp -r ~/ConfigFiles/bspwm/* ~/.config/bspwm
 cp -r ~/ConfigFiles/kitty/* ~/.config/kitty
+cp -r ~/ConfigFiles/dunst/* ~/.config/dunst
+cp -r ~/ConfigFiles/picom/* ~/.config/picom
+
+# Instalar Rofi si el usuario lo desea
+if [ "$choice_rofi" = "S" ]; then
+  cp -r ~/ConfigFiles/rofi/* ~/.config/rofi
+fi
+
 
 echo "Copiando archivos de powerlevel10k y zsh_modulos"
 cp -r ~/ConfigFiles/powerlevel10k ~/
 sudo cp -r ~/ConfigFiles/zsh_modul/zsh-* /usr/share/
-
-echo "Actualizando configuración..."
-echo "Corrigiendo ~/.config/sxhkd/sxhkdrc"
-sed -i 's|urxvt|'"$(which kitty)"'|g' ~/.config/sxhkd/sxhkdrc
-sed -i "s+/opt/kitty/bin/kitty+$(which kitty)+" ~/.config/sxhkd/sxhkdrc
-sed -i 's/super + @space/super + d/g' ~/.config/sxhkd/sxhkdrc
-sed -i 's/dmenu_run/rofi -show run/g' ~/.config/sxhkd/sxhkdrc
-
-echo "Corrigiendo ~/.config/bspwm/bspwmrc"
-sed -i "s/picom &/#picom &/" ~/.config/bspwm/bspwmrc
-#sed -i 's/pgrep -x sxhkd > \/dev\/null || sxhkd &/pkill sxhkd\nsxhkd \&/' ~/.config/bspwm/bspwmrc
-sed -i "s+/usr/share/custonTheme/hell_wallpaper.jpg+~/WallPapers/Wall_OnePiece.png+g" ~/.config/bspwm/bspwmrc
 
 echo "Corrigiendo ~/.zshrc"
 sed -i "s/alias cat='batcat'/alias cat='bat'/" ~/.zshrc
@@ -141,15 +214,19 @@ echo "Copia archivos a Root"
 sudo mkdir -p /root/.config/nvim
 sudo mkdir -p /root/powerlevel10k
 
+#sudo cp -R ~/.config /root/.config
+
 sudo ln -s ~/.zshrc /root/.zshrc
 sudo ln -s ~/.p10k.zsh  /root/.p10k.zsh
 sudo cp -r ~/ConfigFiles/nvim/* /root/.config/nvim
 sudo cp -r ~/powerlevel10k/* /root/powerlevel10k
 
 echo "Otorgando permisos de ejecución..."
-find ~/.config -type f -name "*.sh" -exec bash -c 'echo "Modificando permisos de: $1"; chmod +x "$1"' bash {} \;
-for archivo in ~/.config/polybar/scripts/*; do echo "Modificando permisos de: $archivo"; chmod +x "$archivo"; done
-
+chmod +x ~/.config/bspwm/scripts/*
+chmod +x ~/.config/bspwm/*
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/scripts/*
+chmod +x ~/.config/bin/*
 
 # Eliminar archivos Innecesarios
 sudo rm -rf /usr/share/fonts/nerd-fonts/{*.zip,*.md}
